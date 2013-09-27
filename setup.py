03157e3ee1eb6a7d4f8af9892586d8eee3a50607
zaurky <zaurky@zeb.re>
2013-09-27 11:31:03 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from os import path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+README = path.abspath(path.join(path.dirname(__file__), 'README.md'))
+desc = 'A Python util to easily download videos from vine'
+
+setup(
+    name='vine-dwl',
+    version='0.0.1',
+    author='Zaurky',
+    author_email='zaurky@zeb.re',
+    description=desc,
+    long_description=open(README).read(),
+    license='GPLV2',
+    url='http://github.com/zaurky/vine_dwl',
+    packages=['vine-dwl'],
+    install_requires=[
+        'BeautifulSoup',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
